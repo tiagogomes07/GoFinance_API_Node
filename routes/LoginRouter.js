bdd0e87ada0f77b8b8000b68918e2931acb38972
Tiago Gomes <tiago_gomes07@hotmail.com>
2018-03-03 13:56:16 -0300

    implantando login de acesso

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Sequelize = require('sequelize');
 const connection = require('../dal/Sequelize')();
+//var Q = require('q');
 
 router.get('/', function(req, res, next) {  
     
@@ -31,5 +32,58 @@ router.get('/', function(req, res, next) {
 
     res.send("Ok");
 });
+
+router.post('/create',function(req, res, next) {  
+    
+    var Login = require('../models/Login');
+    var Login = Login(connection,Sequelize.DataTypes);
+
+    connection.sync().then( function(){
+        
+       VerificarLogin(Login,req).then(function(result){
+            if(result == false){              
+               
+                Login.create(
+                { PrimeiroNome : req.body.PrimeiroNome,
+                    SobreNome : req.body.SegundoNome, 
+                    UserName : req.body.UserName, 
+                    Password : req.body.Password }); 
+
+                res.send({'mensage':'salvo com sucesso'});    
+            }else{
+                res.send({'mensage':'Este login já está em uso'}); 
+            }
+       })
+
+        //verificar se já existe este login no banco de dados  
+        //      console.log('created');
+
+        // Login.findById(2).then(function(Login){
+        //     console.log(Login.dataValues);
+        // })
+
+        Login.findAll().then(function(Login){
+           // console.log(Login);
+           // console.log(Login.dataValues);
+        })
+
+    })
+
+});
+
+function VerificarLogin (Login,req){
+   return Login.findAll({
+            where:{ UserName: req.body.UserName}
+        })
+        .then( function(response) {
+            console.log({'Ja existe login?':response});        
+            if(response.length == 0){            
+                return false;
+            }else{            
+                return true;
+            }  
+        });
+}
+
 
 module.exports = router;
